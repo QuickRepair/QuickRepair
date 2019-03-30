d983bbb96a7762e86d73eaaca33b254cb7e7d518
whywhywhywhygo <whywhywhywhygo@gmail.com>
2019-03-30 12:40:48 +0800

    添加启动时自动登陆功能

--- app/src/main/java/com/har/quickrepairforandroid/NavigationActivity.java
+++ app/src/main/java/com/har/quickrepairforandroid/NavigationActivity.java
@@ -1,7 +1,10 @@
 package com.har.quickrepairforandroid;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
@@ -10,10 +13,21 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.widget.Toast;
 
+import com.har.quickrepairforandroid.AsyncTransmissions.AsyncTransmissionTask;
+import com.har.quickrepairforandroid.AsyncTransmissions.HttpConnection;
 import com.har.quickrepairforandroid.Database.AccountBaseHelper;
 import com.har.quickrepairforandroid.Database.AccountCursorWrapper;
 import com.har.quickrepairforandroid.Models.AccountHolder;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 
 public class NavigationActivity extends AppCompatActivity {
 
@@ -25,6 +39,8 @@ public class NavigationActivity extends AppCompatActivity {
 	private BottomNavigationView navigation;
 	private ViewPager fragmentPagerContainer;
 
+	private Handler mMainHandler;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -35,6 +51,8 @@ public class NavigationActivity extends AppCompatActivity {
 		orderListFragment = OrderListFragment.newInstance();
 		messageListFragment = MessageListFragment.newInstance();
 		aboutMeFragment = AboutMeFragment.newInstance();
+
+		mMainHandler = new Handler();
 
 		fm = getSupportFragmentManager();
 		fragmentPagerContainer.setAdapter(new FragmentPagerAdapter(fm) {
@@ -102,15 +120,68 @@ public class NavigationActivity extends AppCompatActivity {
 		});
 
 		readAccountFromDatabase();
+		new AutoLoginTask().execute();
 	}
 
 	private void readAccountFromDatabase() {
 		SQLiteDatabase database = new AccountBaseHelper(getApplicationContext()).getReadableDatabase();
 		AccountCursorWrapper cursor = AccountHolder.getInstance().queryAccount(database);
+		cursor.moveToFirst();
 		if(!cursor.isAfterLast()) {
 			AccountHolder.getInstance().setAccount(cursor.getAccount());
 			AccountHolder.getInstance().setPassword(cursor.getPassword());
 			AccountHolder.getInstance().setIsCustomer(cursor.getIsCustomer());
+		}
+	}
+
+	private class AutoLoginTask implements AsyncTransmissionTask {
+		@Override
+		public void execute() {
+			HttpConnection.getInstance().postMethod(makeRequest(), this);
+		}
+
+		@Override
+		public Request makeRequest() {
+			try {
+				JSONObject json = new JSONObject();
+				json.put("type", "login");
+				json.put("account", AccountHolder.getInstance().getAccount());
+				json.put("password", AccountHolder.getInstance().getPassword());
+				json.put("account_type", AccountHolder.getInstance().getIsCustomer() ? "customer" : "merchant");
+				RequestBody requestBody = RequestBody.create(AsyncTransmissionTask.TypeJson, json.toString());
+				return new Request.Builder().url(getApplicationContext().getResources().getString(R.string.server_ip)).post(requestBody).build();
+			} catch (JSONException je) {
+				je.printStackTrace();
+				return null;
+			}
+		}
+
+		@Override
+		public void handler(Response response) {
+			try{
+				String findJson = response.body().string();
+				JSONObject jsonObject = new JSONObject(findJson);
+				final String loginResult = jsonObject.getString("login_result");
+
+				mMainHandler.post(new Runnable() {
+					@Override
+					public void run() {
+						if(loginResult.equalsIgnoreCase("success")) {// login successed
+							AccountHolder.getInstance().setIsLogin(true);
+							// make toast
+							Toast.makeText(NavigationActivity.this, R.string.login_successsed, Toast.LENGTH_SHORT).show();
+						} else if(loginResult.equalsIgnoreCase("no such an account")){  // login failed: no account
+							Toast.makeText(NavigationActivity.this, R.string.login_failed_no_such_account, Toast.LENGTH_SHORT).show();
+						} else if(loginResult.equalsIgnoreCase("wrong password")) {     //login failed: password wrong
+							Toast.makeText(NavigationActivity.this, R.string.login_failed_wrong_password, Toast.LENGTH_SHORT).show();
+						}
+					}
+				});
+			} catch (JSONException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 }
